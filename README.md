115af76817366433efadf2ca2f7e358de4994cb6
Victor <victor.vhrf@gmail.com>
2021-04-23 21:46:19 -0400

    implementacao do websocket na semana do nlw com a equipe da rocketseat

---
+++ README.md
@@ -0,0 +1,22 @@
+<h1 align="center">
+    <img alt="Semana Omnistack" src="public/images/rocketseat.svg" width="400px" />
+</h1>
+
+# Next Level Week
+
+Bootcamp performed by rocketseat to take the next level in React development
+
+In this week, we development a websocket application fully function whitout a beautiful layout. The biggest focus is funtionality.
+
+This websocket works as a support chat with a client and an admin with exchanges of messages simultaneously.
+
+### 🛠 Tecnologias
+
+The following techonologie were used:
+-[Node.js](https://nodejs.org/en/)
+
+some React commands:
+
+### `yarn dev`
+Runs the app in the development mode.\
+Open [http://localhost:3000](http://localhost:3000) to view it in the browser.
---
+++ src/services/UsersService.ts
@@ -0,0 +1,39 @@
+import { getCustomRepository, Repository } from "typeorm";
+import { User } from "../entities/User";
+import { UsersRepository } from "../repositories/UsersRepository"
+
+
+class UsersService {
+  private usersRepository: Repository<User>
+
+  constructor() {
+    this.usersRepository = getCustomRepository(UsersRepository);
+  }
+  async create(email: string) {
+    // verificar se o usuario existe 
+    const userExists = await this.usersRepository.findOne({
+      email
+    })
+
+    //se nao existir, salvar no DB
+    if (userExists) {
+      return userExists;
+    }
+
+    const user = this.usersRepository.create({
+      email
+    });
+
+    await this.usersRepository.save(user);
+    //se existir, retornar user
+
+    return user;
+  }
+  async findByEmail(email: string) {
+    const user = await this.usersRepository.findOne({ email });
+
+    return user;
+  }
+}
+
+export { UsersService }
